e475310b0769bd57ff6e9d7a8f5b84ac28ea91eb
Will Cosh <dsdbusiness01@gmail.com>
2023-10-09 10:40:36 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+p7911-D4E2lugins {
+    id("java")
+}
+
+group = "com.dsd.st"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "survival_trials"
+
